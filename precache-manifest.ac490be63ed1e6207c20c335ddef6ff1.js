0113b25d51040d6b0ef068756363f94fab51225b
GoldenDude <edanazran@gmail.com>
2020-04-27 09:28:55 +0300

    Updates

---
+++ precache-manifest.ac490be63ed1e6207c20c335ddef6ff1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf29e4e8a66c2d077b8aefc8535909d4",
+    "url": "/yarr/index.html"
+  },
+  {
+    "revision": "9feb99c26e57eebd446b",
+    "url": "/yarr/static/css/2.7efce84c.chunk.css"
+  },
+  {
+    "revision": "6c79bbc52e8150a15def",
+    "url": "/yarr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9feb99c26e57eebd446b",
+    "url": "/yarr/static/js/2.0a82ec85.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/yarr/static/js/2.0a82ec85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c79bbc52e8150a15def",
+    "url": "/yarr/static/js/main.91a8640f.chunk.js"
+  },
+  {
+    "revision": "c81384e07fe71ae3802c",
+    "url": "/yarr/static/js/runtime-main.6815ee23.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/yarr/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/yarr/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "de97ae4c59ace483e8ebd107eb655b7b",
+    "url": "/yarr/static/media/blue_yellow_color_blindness.de97ae4c.jpg"
+  },
+  {
+    "revision": "0ae8f2c13e414eff20292116bd02d133",
+    "url": "/yarr/static/media/competitive.0ae8f2c1.png"
+  },
+  {
+    "revision": "9cc01480e71afa0e2f4892f2ebddc1a7",
+    "url": "/yarr/static/media/cooperative.9cc01480.png"
+  },
+  {
+    "revision": "7df4a3fe6274488ddd16e0b9fa7e38eb",
+    "url": "/yarr/static/media/different_colors.7df4a3fe.png"
+  },
+  {
+    "revision": "a6d68a520f4e3c31ac5032119ae679d3",
+    "url": "/yarr/static/media/different_design.a6d68a52.png"
+  },
+  {
+    "revision": "8fa2a68f1c70ddfe0ed31d0295057b7f",
+    "url": "/yarr/static/media/different_shapes.8fa2a68f.png"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/yarr/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/yarr/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/yarr/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/yarr/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/yarr/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/yarr/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/yarr/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/yarr/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/yarr/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/yarr/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/yarr/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/yarr/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/yarr/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/yarr/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/yarr/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "b6b770bfcf0c725aa716aa0b2d8a50a0",
+    "url": "/yarr/static/media/full_spectrum_vision.b6b770bf.jpg"
+  },
+  {
+    "revision": "2490b99e58d823fb5cc70a8280d292be",
+    "url": "/yarr/static/media/red_green_color_blindness.2490b99e.jpg"
+  }
+]);
